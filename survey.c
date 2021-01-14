7ad6e974129df6e98fe27645d1e27657eba89f12
Danil Bychkov <d.bychkov@metrotek.ru>
2021-01-14 16:04:46 +0300

    Option parsing

--- survey.c
+++ survey.c
@@ -1,18 +1,49 @@
-/*
-survey
-
-survey -h
-survey --help
-
-survey -f "%4d %3n %16d" 145
-survey --format="%4d %3n %16d" 145
-*/
-
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello World!\n");
-    return 0;
-  }
+int
+main (int argc, char **argv)
+{
+  int aflag = 0;
+  int bflag = 0;
+  char *format = NULL;
+  int index;
+  int c;
 
+  opterr = 0;
 
+  while ((c = getopt (argc, argv, "abc:")) != -1)
+    switch (c)
+      {
+      case 'a':
+        aflag = 1;
+        break;
+      case 'b':
+        bflag = 1;
+        break;
+      case 'c':
+        format = optarg;
+        break;
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+
+  printf ("aflag = %d, bflag = %d, format = %s\n",
+          aflag, bflag, format);
+
+  for (index = optind; index < argc; index++)
+    printf ("Non-option argument %s\n", argv[index]);
+  return 0;
+}

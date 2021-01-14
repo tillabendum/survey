19c4c4d97e2e67a9c1ef445d843f215e8ecd11e0
Danil Bychkov <d.bychkov@metrotek.ru>
2021-01-14 17:05:45 +0300

    Pritimiteve format parsing

--- survey.c
+++ survey.c
@@ -1,11 +1,25 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 
-int
-main (int argc, char **argv)
+typedef struct{
+  enum {LITERAL, HEX, DEC, BIN, NIHIL} type;
+  char *literal;
+  int  num_digits;
+  } record_t;
+
+// Prototypes
+void print_record( record_t );
+
+
+int main (int argc, char **argv)
 {
+////////////////////////////////////////////////////////////////////////////////
+// Parsing options
+////////////////////////////////////////////////////////////////////////////////
   int aflag = 0;
   int bflag = 0;
   char *format = NULL;
@@ -40,10 +54,83 @@ main (int argc, char **argv)
         abort ();
       }
 
-  printf ("aflag = %d, bflag = %d, format = %s\n",
-          aflag, bflag, format);
+  printf ("%d%d format = %s\n", aflag, bflag, format);
 
   for (index = optind; index < argc; index++)
     printf ("Non-option argument %s\n", argv[index]);
+
+////////////////////////////////////////////////////////////////////////////////
+// Analyzing format
+////////////////////////////////////////////////////////////////////////////////
+
+printf("Analyzing format:\n");
+size_t length = strlen( format );
+
+bool  start = false;
+for (size_t i = 0; i < length; i++)
+  {
+    printf("%c:", format[i]);
+
+    if ( format[i] == '%' )
+      {
+        printf("start\n");
+        start = true;
+      }
+    else if ( start )
+      {
+        switch( format[i] )
+          {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+              printf("number\n");
+              break;
+            case 'h':
+              printf("hex\n");
+              start =false;
+              break;
+            case 'd':
+              printf("dec\n");
+              start =false;
+              break;
+            case 'b':
+              printf("bin\n");
+              start =false;
+              break;
+            case 'n':
+              printf("nihil\n");
+              start =false;
+              break;
+            default:
+              printf("unexpected format\n");
+              return 1;
+          }
+      }
+    else
+      {
+      printf("literal\n");
+      }
+  }
+
   return 0;
 }
+
+
+
+void print_record( record_t record)
+  {
+    if( record.type == LITERAL )
+      printf("Literal:");
+    else if ( record.type == HEX )
+      printf("HEX:");
+    else
+      printf("UNKNOWN");
+
+  }

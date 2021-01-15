f1ba69369e49e473491551c96b97bff4450ee0c6
Danil Bychkov <d.bychkov@metrotek.ru>
2021-01-15 14:44:40 +0300

    Cpp build recovered

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <unistd.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+typedef enum {LITERAL, HEX, DEC, BIN, NIHIL} record_type_t;
+
 class Record{
-    typedef enum {LITERAL, HEX, DEC, BIN, NIHIL} record_type_t;
     public:
       Record();
 
@@ -21,6 +23,7 @@ Record::Record(){
 };
 
 
+int make_number_record( string buf, record_type_t type, vector<Record> * );
 
 int main (int argc, char **argv)
   {
@@ -63,13 +66,110 @@ int main (int argc, char **argv)
     ////////////////////////////////////////////////////////////////////////////////
     cout << "Analyzing format\n";
     string format = string( cformat );
+    vector<Record> records;
 
-    Record record;
+    size_t  length = format.size();
+    bool    start = false;
+    string  buf;
 
+    for (size_t i = 0; i < length; i++)
+      {
+        printf("%c:", format[i]);
 
-    cout << format << endl;
+        if ( format[i] == '%' )
+          {
+            if( !buf.empty() )
+              {
+                cout << buf;
+                buf.clear();
+              }
+            printf("start\n");
+            start = true;
+          }
+        else if ( start )
+          {
+            switch( format[i] )
+              {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                  buf += format[i];
+                  break;
+                case 'h':
+                  if( make_number_record( buf, HEX, &records ) )
+                    return 1;
+                  start =false;
+                  break;
+                case 'd':
+                  if( make_number_record( buf, DEC, &records ) )
+                    return 1;
+                  start =false;
+                  break;
+                case 'b':
+                  if( make_number_record( buf, BIN, &records ) )
+                    return 1;
+                  start =false;
+                  break;
+                case 'n':
+                  if( make_number_record( buf, NIHIL, &records ) )
+                    return 1;
+                  start =false;
+                  break;
+                default:
+                  printf("unexpected format\n");
+                  return 1;
+              }
+          }
+        else
+          {
+          buf += format[i];
+          }
+      }
 
+    if (start)
+      {
+        printf("non-terminated format");
+        return 1;
+      }
+    else if ( !buf.empty() )
+      {
+        cout <<  buf << endl;
+        buf.clear();
+      }
 
-
+    cout << records.size() << endl;
   return 0;
+  } // main
+
+
+
+int make_number_record( string buf, record_type_t type, vector<Record> *records )
+  {
+    if( !buf.empty() )
+      {
+        int value;
+        value = std::stoi( buf, 0, 10 );
+        Record  record;
+        record.type = type;
+        record.num_digits = value;
+        records->push_back( record );
+        printf("create number with %d digits\n", value);
+        buf.clear();
+      }
+    else
+      {
+        printf("Invalid format\n");
+        return 1;
+      }
+    return 0;
   }
+
+
+
